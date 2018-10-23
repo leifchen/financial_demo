b20d7b823adc43d06f519cd790da46af059766c4
LeifChen <leifchen90@gmail.com>
2018-10-23 22:03:56 +0800

    style: update swagger

--- swagger/src/main/java/com/chen/swagger/SwaggerConfiguration.java
+++ swagger/src/main/java/com/chen/swagger/SwaggerConfiguration.java
@@ -1,7 +1,6 @@
 package com.chen.swagger;
 
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -27,7 +26,6 @@ public class SwaggerConfiguration {
 
     private final SwaggerInfo swaggerInfo;
 
-    @Autowired
     public SwaggerConfiguration(SwaggerInfo swaggerInfo) {
         this.swaggerInfo = swaggerInfo;
     }
